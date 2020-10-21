bc81dfced61898795f42e61a2336d0124a54ce2e
MoRan2332 <fhuaimin@gmail.com>
2020-10-21 16:44:08 +0800

    text

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@ export default {
 
 <style>
 #app {
+  /* wwa */
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
