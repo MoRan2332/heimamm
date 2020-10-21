f9039f1222b37376d741336446daf867b500e18e
MoRan2332 <fhuaimin@gmail.com>
2020-10-21 17:19:18 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,6 @@ export default {
 
 <style>
 #app {
-  /* wwa */
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
